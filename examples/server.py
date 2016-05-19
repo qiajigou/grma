bb19cb09f115845631b8b1a1327a7936b47267ae
GuoJing <soundbbg@gmail.com>
2016-05-19 20:50:57 +0800

    add examples for gRMA
    1. add simple gRPC server example
    2. add gRPC client to call server
    3. just use gRAM!

---
+++ examples/server.py
@@ -0,0 +1,45 @@
+import time
+import server_pb2
+
+from grma.server.base import ServerBase
+
+ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+class ServiceImpl(server_pb2.BetaSimpleServiceServicer):
+    def hello(self, request, context):
+        say = request.say
+        return server_pb2.HelloResponse(reply='you said: %s' % say)
+
+    Hello = hello
+
+
+class Server(ServerBase):
+    def __init__(self):
+        """Init a r2d2 server instance"""
+        server = server_pb2.beta_create_SimpleService_server(
+            ServiceImpl()
+        )
+        self.server = server
+        self.started = False
+
+    def bind(self, host, port, private_key_path='', certificate_chain_path=''):
+        self.server.add_insecure_port('%s:%s' % (host, port))
+
+    def start(self):
+        """start server"""
+        self.server.start()
+        self.started = True
+        try:
+            while self.started:
+                time.sleep(ONE_DAY_IN_SECONDS)
+        except KeyboardInterrupt:
+            self.stop()
+
+    def stop(self, grace=0):
+        self.server.stop(0)
+        self.started = False
+
+
+# entry point of grma
+app = Server()
