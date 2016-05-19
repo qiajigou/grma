bafcc8484772dc7afc62890a225427f9b7ff1c66
GuoJing <soundbbg@gmail.com>
2016-05-19 13:31:47 +0800

    change version number

--- grma/__init__.py
+++ grma/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -
 
-version_info = (0, 0, 1)
+version_info = (0, 0, 2)
 __version__ = '.'.join([str(i) for i in version_info])
 __logo__ = '''
 ************************************
